71a158d8a1249aab7a8cb9e0ef2f151d0ccff6f0
1ns0mn1a7 <tyrischev.d.v@gmail.com>
2025-05-04 20:24:19 +0300

    Update founded_year_text.py

--- founded_year_text.py
+++ founded_year_text.py
@@ -1,6 +1,11 @@
 from datetime import datetime
 
 
+def calculate_age(start_year):
+    current_year = datetime.now().year
+    return current_year - start_year
+
+
 def get_years_text(age):
     if 11 <= age % 100 <= 14:
         return "лет"
@@ -13,8 +18,7 @@ def get_years_text(age):
         return "лет"
 
 
-def get_founded_text(start_year = 1920):
-    current_year = datetime.now().year
-    age = current_year - start_year
+def get_founded_text(start_year=1920):
+    age = calculate_age(start_year)
     word = get_years_text(age)
     return f"Уже {age} {word} с вами"
