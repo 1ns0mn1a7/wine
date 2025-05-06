00b36fde1fe8ce5c57d2d36c8b4d06a9e0d72c00
1ns0mn1a7 <tyrischev.d.v@gmail.com>
2025-05-06 21:07:00 +0300

    add fixes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 
 def main():
     parser = argparse.ArgumentParser(description='Запустить сайт винодельни.')
-    parser.add_argument('--excel-path', default='wine3.xlsx', help='Путь к Excel-файлу с данными о вине')
+    parser.add_argument('--excel-path', default='wines_catalog.xlsx', help='Путь к Excel-файлу с данными о вине')
     args = parser.parse_args()
 
     excel_wines = pandas.read_excel(args.excel_path, sheet_name='Лист1')
@@ -28,7 +28,7 @@ def main():
 
     rendered_page = template.render(
         founded_text=get_founded_text(),
-        grouped=grouped_wines
+        grouped_wines=grouped_wines
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
